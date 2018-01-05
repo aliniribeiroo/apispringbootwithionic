1479fbc3136d1f28d4d8a8e1cc89b1bad88a2be8
Alini Ribeiro <aliniribeiroo@gmail.com>
2018-01-05 18:46:05 -0200

    Primeiro commit do projeto

---
+++ src/main/java/com/aliniribeiro/APISpringBootWithIonic/ApiSpringBootWithIonicApplication.java
@@ -0,0 +1,12 @@
+package com.aliniribeiro.APISpringBootWithIonic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringBootWithIonicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpringBootWithIonicApplication.class, args);
+	}
+}
